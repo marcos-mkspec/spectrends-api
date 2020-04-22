19340ac8092951f22c09a64c7d4a8dede95df24e
marcos-mkspec <marcos@mkspec.com.br>
2020-04-22 12:55:46 -0300

    limpeza do projeto

--- aspnet-core/src/Mkspec.Spectrends.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Mkspec.Spectrends.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
